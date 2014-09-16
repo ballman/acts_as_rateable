68c94e5037e15af851243472bd66acb168a6e842
Allen Ballman <aballman@gmail.com>
2014-09-16 08:06:31 -0400

    Fixes for deprecation warnings in Rails 4.

--- lib/acts_as_rateable/acts_as_rateable.rb
+++ lib/acts_as_rateable/acts_as_rateable.rb
@@ -19,7 +19,7 @@ module ActiveRecord
 
       module ClassMethods
         def acts_as_rateable(options = {})
-          has_many :ratings, :as => :rateable, :dependent => :destroy, :include => :rate
+          has_many :ratings, -> { includes :rate}, :as => :rateable, :dependent => :destroy
           has_many :rates, :through => :ratings, :extend => AssignRateWithUserId
 
           include ActiveRecord::Acts::Rateable::InstanceMethods
@@ -46,7 +46,7 @@ module ActiveRecord
         #
         def rate_it( score, user, free_text = "" )
           return unless score
-          rate = Rate.find_or_create_by_score( score.to_i )
+          rate = Rate.find_or_create_by(score: score.to_i)
           raise "User must respond to 'id' in order to set the user ID!" unless user.respond_to? :id
           raise "User must respond to 'login' in order to set the rater name!" unless user.respond_to? :login
           rate.user_id = user.id
